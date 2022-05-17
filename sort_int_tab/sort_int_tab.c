19bbf1d14a27de040d3ac73d177732e365e8b722
HenriM <henrimaronen@gmail.com>
2022-05-17 11:58:39 +0300

    sort int tab ready

--- sort_int_tab/sort_int_tab.c
+++ sort_int_tab/sort_int_tab.c
@@ -1,56 +1,20 @@
-#include <stdio.h>
-#include <unistd.h>
-
 void	sort_int_tab(int *tab, unsigned int size)
 {
 	int i;
-	int max;
 	int temp;
 
 	temp = 0;
-	max = 0;
 	i = 0;
-	// 5, 2, 3, 4, 2
-	// 2, 5, 3, 4, 2
-	// 2, 3, 5, 4, 2
-
-	while (i <= size)	
+	while (i < (size - 1))	
 	{
-		if (tab[i] > max)
-		{
-			max = tab[i];
-			i = 0;
-		}
-		else
+		if (tab[i] > tab[i + 1])
 		{
 			temp = tab[i];
-			tab[i] = max;
-			
+			tab[i] = tab[i + 1];
+			tab[i + 1] = temp;
+			i = 0;
 		}
-
 		i++;
 	}
 }
 
-int main(void)
-{
-	int i;
-	i = 0;
-
-	int tab[5] = {5, 2, 3, 4, 2};
-	
-	while (i <= 5)
-	{
-		printf("%d", tab[i]);
-		i++;
-	}
-	printf("\n");
-	sort_int_tab(tab, 5);
-	i = 0;
-	while (i <= 5)
-	{
-		printf("%d", tab[i]);
-		i++;
-	}
-	printf("\n");
-}
